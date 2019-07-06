173e2f5357daaa854af4b20db88d6325cf43dac6
Leonardo <leonardo@codeninjas.pl>
2019-07-06 22:50:51 +0200

    smtp mailer

---
+++ system/Application/Event/EventDispatcherAdapter.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace System\Application\Event;
+
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Adapter to meet contract breaking change is Symfony 4.3:
+ * Calling the "Symfony\Component\EventDispatcher\EventDispatcherInterface::dispatch()" method with the event name
+ * as first argument is deprecated since Symfony 4.3, pass it second and provide the event object first instead.
+ */
+class EventDispatcherAdapter implements EventDispatcherInterface
+{
+    private $dispatcher;
+
+    public function __construct(EventDispatcherInterface $eventDispatcher)
+    {
+        $this->dispatcher = $eventDispatcher;
+    }
+
+    public function addListener($eventName, $listener, $priority = 0)
+    {
+        $this->dispatcher->addListener($eventName, $listener, $priority);
+    }
+
+    public function addSubscriber(EventSubscriberInterface $subscriber)
+    {
+        $this->dispatcher->addSubscriber($subscriber);
+    }
+
+    public function removeListener($eventName, $listener)
+    {
+        $this->dispatcher->removeListener($eventName, $listener);
+    }
+
+    public function removeSubscriber(EventSubscriberInterface $subscriber)
+    {
+        $this->dispatcher->removeSubscriber($subscriber);
+    }
+
+    public function getListeners($eventName = null)
+    {
+        return $this->dispatcher->getListeners($eventName);
+    }
+
+    public function getListenerPriority($eventName, $listener)
+    {
+        return $this->dispatcher->getListenerPriority($eventName, $listener);
+    }
+
+    public function hasListeners($eventName = null)
+    {
+        return $this->dispatcher->hasListeners($eventName);
+    }
+
+    public function dispatch($event)
+    {
+        return $this->dispatcher->dispatch(func_get_arg(1), func_get_arg(0));
+    }
+}
---
+++ system/Infractructure/Mailer/ZendMail/SmtpMailer.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace System\Infractructure\Mailer\ZendMail;
+
+use Zend\Mail\Message;
+use Zend\Mail\Transport\Smtp;
+use Zend\Mail\Transport\SmtpOptions;
+use System\Infractructure\Mailer\ZendMail\MailerInterface;
+
+class SmtpMailer implements MailerInterface
+{
+    private $host;
+
+    private $port;
+
+    private $username;
+
+    private $password;
+
+    public function __construct(string $host, string $port, string $username, string $password)
+    {
+        $this->host     = $host;
+        $this->port     = $port;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function send(Message $message): void
+    {
+        $transport = new Smtp();
+        $options   = new SmtpOptions(array(
+            'host'              => $this->host,
+            'port'              => $this->port,
+            'connection_class'  => 'login',
+            'connection_config' => [
+                'username' => $this->username,
+                'password' => $this->password,
+            ],
+        ));
+
+        $transport->setOptions($options);
+        $transport->send($message);
+    }
+}
