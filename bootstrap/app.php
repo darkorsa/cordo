7f0ab21ddacb64935827840d1c2e59bb0508a7ee
Dariusz Korsak <d.korsak@codeninjas.pl>
2019-11-09 13:59:03 +0100

    formatting fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,17 +9,17 @@ use Symfony\Component\Dotenv\Dotenv;
 use System\Infractructure\Mailer\ZendMail\MailerFactory;
 
 $dotenv = new Dotenv();
-$dotenv->load(root_path().'.env');
+$dotenv->load(root_path() . '.env');
 
 // Errors
-$errorReporter = require __DIR__.'/error.php';
+$errorReporter = require __DIR__ . '/error.php';
 
 // DI container Psr\Container\ContainerInterface
 $containerBuilder = new ContainerBuilder();
 $containerBuilder->addDefinitions(Loader::loadDefinitions());
 
 if (getenv('APP_ENV') === ENV_PRODUCTION) {
-    $containerBuilder->enableCompilation(storage_path().'cache');
+    $containerBuilder->enableCompilation(storage_path() . 'cache');
 }
 
 $container = $containerBuilder->build();
@@ -29,7 +29,7 @@ $container->set('error_reporter', $errorReporter);
 Loader::loadConfigs($container->get('config'));
 
 // Acl
-$acl = require __DIR__.'/acl.php';
+$acl = require __DIR__ . '/acl.php';
 $container->set('acl', $acl);
 
 // Mailer
@@ -37,11 +37,11 @@ $mailer = MailerFactory::factory($container->get('config')->get('mail'));
 $container->set('mailer', $mailer);
 
 // Database
-$entityManager = require __DIR__.'/db.php';
+$entityManager = require __DIR__ . '/db.php';
 $container->set('entity_manager', $entityManager);
 
 // Command bus
-$commandBus = require __DIR__.'/command_bus.php';
+$commandBus = require __DIR__ . '/command_bus.php';
 $container->set('command_bus', $commandBus);
 
 return $container;
