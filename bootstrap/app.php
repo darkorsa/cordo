6d8ce6d6386110714e1a52dfd6f553097f3c1288
Dariusz Korsak <d.korsak@codeninjas.pl>
2019-11-30 11:01:42 +0100

    UUID configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,13 +1,30 @@
 <?php
 
 use App\Register;
+use Ramsey\Uuid\Uuid;
 use DI\ContainerBuilder;
+use Ramsey\Uuid\FeatureSet;
+use Ramsey\Uuid\UuidFactory;
 use System\SharedKernel\Enum\Env;
 use Symfony\Component\Dotenv\Dotenv;
+use Ramsey\Uuid\Generator\DefaultTimeGenerator;
+use Ramsey\Uuid\Converter\Time\PhpTimeConverter;
+use Ramsey\Uuid\Provider\Node\RandomNodeProvider;
+use Ramsey\Uuid\Provider\Time\SystemTimeProvider;
 use System\Infractructure\Mailer\ZendMail\MailerFactory;
 
 $dotenv = new Dotenv();
 $dotenv->load(root_path() . '.env');
+
+// UUID config
+$uuidFactory = new UuidFactory(new FeatureSet(false, false, false, true, false));
+$uuidFactory->setTimeGenerator(new DefaultTimeGenerator(
+    new RandomNodeProvider(),
+    new PhpTimeConverter(),
+    new SystemTimeProvider()
+));
+
+Uuid::setFactory($uuidFactory);
 
 // Errors
 $errorReporter = require __DIR__ . '/error.php';
@@ -17,6 +34,7 @@ $errorReporter = require __DIR__ . '/error.php';
  */
 $containerBuilder = new ContainerBuilder();
 $containerBuilder->addDefinitions(Register::registerDefinitions());
+$containerBuilder->useAutowiring(true);
 
 if (getenv('APP_ENV') === Env::PRODUCTION()) {
     $containerBuilder->enableCompilation(storage_path() . 'cache');
