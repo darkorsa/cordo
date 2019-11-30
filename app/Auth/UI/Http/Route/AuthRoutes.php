1f0c2544f3af61a431bb80f87bd57c503d5ee291
Dariusz Korsak <d.korsak@codeninjas.pl>
2019-11-30 13:53:24 +0100

    apidoc for auth endpoint

--- app/Auth/UI/Http/Route/AuthRoutes.php
+++ app/Auth/UI/Http/Route/AuthRoutes.php
@@ -18,6 +18,27 @@ class AuthRoutes extends RoutesRegister
 
     private function addOauthToken(): void
     {
+        /**
+         * @api {post} /token Authorize user
+         * @apiName AuthToken
+         * @apiGroup Auth
+         *
+         * @apiParam {String} [username] Username
+         * @apiParam {String} [password] Password
+         * @apiParam {String} [grant_type] Grant type (should have value 'password')
+         * @apiParam {String} [client_id] Client ID - default value 'Cordo', can ba changed in oauth_clients db table
+         *
+         * @apiSuccessExample Success-Response:
+         * HTTP/1.1 200 OK
+         * {
+         *   "access_token": "f5dee6f234b6ac0333958643f6728736f812513a",
+         *   "expires_in": 3600,
+         *   "token_type": "Bearer",
+         *   "scope": null,
+         *   "refresh_token": "6edc70d399c9594c693429554ae9067d49735419",
+         *   "login": "user@email.com"
+         * }
+         */
         $this->router->addRoute(
             'POST',
             '/token',
