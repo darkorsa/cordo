a024a2066d56f429521689c5be1a591cf6c35ae7
Dariusz Korsak <d.korsak@codeninjas.pl>
2021-08-30 12:03:32 +0200

    validation messages: polish translation

---
+++ tests/Unit/ValidatiorTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use Cordo\Core\UI\Validator\AbstractValidator;
+
+function createValidator(array $data, array $customDefaultMessages = null)
+{
+    return new class($data, $customDefaultMessages) extends AbstractValidator
+    {
+        protected function validationRules(): void
+        {
+            $this->validator
+                ->required('email')
+                ->email()
+                ->length(15);
+        }
+    };
+}
+
+test('validation with custom messages', function () {
+    $data = [
+        'email' => 'test@example',
+    ];
+    $customMessages = require resources_path('lang/pl/validation.php');
+    $validator = createValidator($data, $customMessages);
+
+    expect($validator->isValid())->toBeFalse();
+    expect($validator->messages())->toEqual([
+        'email' => [
+            'Email::INVALID_VALUE' => 'Niepoprawny adres email',
+            'Length::TOO_SHORT' => 'Ilość znaków jest zbyt mała i powinna mieć wartość 15',
+        ],
+    ]);
+});
